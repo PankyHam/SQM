b80ddc644c5b854848eca67b47ac43f0b38004dd
Simon Furber <simon.furber1@gmail.com>
2014-12-12 14:00:06 +0000

    Added server capabilities for getting client ID and room

--- src/main/java/com/sqm/Server.java
+++ src/main/java/com/sqm/Server.java
@@ -1,46 +1,49 @@
 package com.sqm;
 
 import java.io.File;
-import java.nio.file.Paths;
-
 import org.vertx.java.core.Handler;
-import org.vertx.java.core.eventbus.Message;
 import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.core.http.RouteMatcher;
 import org.vertx.java.core.http.ServerWebSocket;
 import org.vertx.java.platform.Verticle;
 
 public class Server extends Verticle {
-
-  public void start() {
-
-	  // Test comment for commit
 	
-		  RouteMatcher httpRouteMatcher = new RouteMatcher().get("/", new
-	  			Handler<HttpServerRequest>() {
-	  				@Override
-	  				public void handle(final HttpServerRequest request) {
-	  					request.response().sendFile("web/index.html");
-	  				}
-	  			}).get(".*\\.(css|js|woff|html)$", new Handler<HttpServerRequest>() {
-	  				@Override
-	  				public void handle(final HttpServerRequest request) {
-	  					request.response().sendFile("web/" + new File(request.path()));
-	  				}
-	  			});
-		  
-		  vertx.createHttpServer().requestHandler(httpRouteMatcher).listen(8080, "localhost");
-		  
-		  Handler<ServerWebSocket> wsHandler = new Handler<ServerWebSocket>() {
+	public void start() {
+	
+		// Matcher to handle http requests on the web client
+		RouteMatcher httpRouteMatcher = new RouteMatcher().get("/",
+				new Handler<HttpServerRequest>() {
+					@Override
+					public void handle(final HttpServerRequest request) {
+						request.response().sendFile("web/index.html");
+					}
+				}).get(".*\\.(css|js|woff|html)$",
+				new Handler<HttpServerRequest>() {
+					@Override
+					public void handle(final HttpServerRequest request) {
+						request.response().sendFile(
+								"web/" + new File(request.path()));
+					}
+				});
+
+		vertx.createHttpServer().requestHandler(httpRouteMatcher)
+				.listen(8080, "localhost");
+
+		// Matcher to handle requests on the web socket
+		Handler<ServerWebSocket> wsHandler = new Handler<ServerWebSocket>() {
 
 			@Override
-			public void handle(ServerWebSocket arg0) {
-				// TODO Auto-generated method stub
-				container.logger().info("Received connection");
+			public void handle(ServerWebSocket connection) {
+				final String client = connection.textHandlerID();
+				final String room = connection.path();
+				
+				container.logger().info("WS Received connection from " + client);
+				container.logger().info(connection.textHandlerID() + "has joined room " + room);
 			}
-			  
-		  };
-		  
-		  vertx.createHttpServer().websocketHandler(wsHandler).listen(8090); 
-  }
+
+		};
+
+		vertx.createHttpServer().websocketHandler(wsHandler).listen(8090);
+	}
 }
