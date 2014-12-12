3cc3accf0076e50be81ec61cd6fb2a80dd16b7f9
Simon Furber <simon.furber1@gmail.com>
2014-12-12 11:32:08 +0000

    Adding websocket support to server, sorting problem chat

--- src/main/java/com/sqm/Server.java
+++ src/main/java/com/sqm/Server.java
@@ -7,6 +7,7 @@ import org.vertx.java.core.Handler;
 import org.vertx.java.core.eventbus.Message;
 import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.core.http.RouteMatcher;
+import org.vertx.java.core.http.ServerWebSocket;
 import org.vertx.java.platform.Verticle;
 
 public class Server extends Verticle {
@@ -14,21 +15,32 @@ public class Server extends Verticle {
   public void start() {
 
 	  // Test comment for commit
+	
+		  RouteMatcher httpRouteMatcher = new RouteMatcher().get("/", new
+	  			Handler<HttpServerRequest>() {
+	  				@Override
+	  				public void handle(final HttpServerRequest request) {
+	  					request.response().sendFile("web/index.html");
+	  				}
+	  			}).get(".*\\.(css|js|woff|html)$", new Handler<HttpServerRequest>() {
+	  				@Override
+	  				public void handle(final HttpServerRequest request) {
+	  					request.response().sendFile("web/" + new File(request.path()));
+	  				}
+	  			});
+		  
+		  vertx.createHttpServer().requestHandler(httpRouteMatcher).listen(8080, "localhost");
+		  
+		  Handler<ServerWebSocket> wsHandler = new Handler<ServerWebSocket>() {
 
-	  RouteMatcher httpRouteMatcher = new RouteMatcher().get("/", new
-  			Handler<HttpServerRequest>() {
-  				@Override
-  				public void handle(final HttpServerRequest request) {
-  					request.response().sendFile("web/index.html");
-  				}
-  			}).get(".*\\.(css|js|woff|html)$", new Handler<HttpServerRequest>() {
-  				@Override
-  				public void handle(final HttpServerRequest request) {
-  					request.response().sendFile("web/" + new File(request.path()));
-  				}
-  			});
-	  
-	  vertx.createHttpServer().requestHandler(httpRouteMatcher).listen(8080, "localhost");
-
+			@Override
+			public void handle(ServerWebSocket arg0) {
+				// TODO Auto-generated method stub
+				System.out.println("Received connection");
+			}
+			  
+		  };
+		  
+		  vertx.createHttpServer().websocketHandler(wsHandler).listen(8090); 
   }
 }
